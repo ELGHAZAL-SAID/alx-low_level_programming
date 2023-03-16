c5aedcda732f8aded66a5eb14a1bdd79ce528406
ELGHAZAL-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-16 15:12:00 -0700

    printing message using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/*
+ * main : Main function => print using printf 
+ *
+ * Return : return 0 
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
+ 
+
