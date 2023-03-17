14da6f3d34ec8cf71c479a0846e4e8906a85a142
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-17 19:49:19 +0100

    lower case letter printing

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main - main fucntion for lowercase printing
+*
+*Return: return 0 every-time
+*/
+
+int main(void)
+{
+
+	int ch;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+
+}
+
