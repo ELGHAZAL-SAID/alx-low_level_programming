f46fad7cb894d66f9f3e298115efd6caac2996be
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-17 19:21:51 +0100

    check last digit of a num

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,11 +10,11 @@
 int main(void)
 {
 
-	int n;
+    int n;
     int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
     i = n % 10;
 
 	if (i > 5)
