e7ec4f7c20942f6a190263d9514d5451d6fcd8f8
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-17 20:15:00 +0100

    lower and upper case letter printing

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main - main function for printing upper and lower case chr
+*
+*Return: return 0 every-time
+*
+*/
+
+int main (void){
+
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+		putchar((int)ch-32);
+	}
+	putchar("\n");
+	return (0);
+
+}
+
