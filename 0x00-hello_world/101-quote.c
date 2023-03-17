6004a0f99e899bfb6a62d583d64c53cdde4dac05
ELGHAZAL-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-16 17:00:57 -0700

    standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - main function -> print quote
+ *
+ *Return : return 1
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+}
