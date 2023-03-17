7ad7b9a0b57cfc02de6ea46a50e7f896e075c2b5
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-17 22:37:02 +0100

    print base 16 nums

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+*main - main function -> print base 16 numbers
+*
+*Return: return 0 every-time
+*/
+
+int main(void)
+{
+
+	int ch;
+
+	for (ch = '1';ch <= '9'; ch++)
+	{
+		putchar(ch);
+
+		if (ch == '9')
+		{
+			for (ch = 'a'; ch <= 'f'; ch ++)				putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
+
