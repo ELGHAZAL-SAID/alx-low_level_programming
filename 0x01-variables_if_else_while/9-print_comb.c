880232c69f1279ebaabb1ea53859f2d4434ebc91
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-17 22:51:46 +0100

    print 1,2,->9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+*main - main function - print 1, -> 9
+*
+*Return: return 0
+*/
+
+int main(void)
+{
+
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+		if (ch != 9)
+		{
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	retrun (0);
+
+}
+
