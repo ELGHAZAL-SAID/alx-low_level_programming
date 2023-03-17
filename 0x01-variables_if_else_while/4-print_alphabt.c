1bbf435d544c40c8617608a943262d25847511e7
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-17 21:10:14 +0100

    all letters except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+*
+*main - main function => all letters except q and e
+*
+*Return: return 0 every-time
+*
+*/
+
+int main(void)
+{
+
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'q' || ch == 'e')
+			continue;
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+
+}
+
