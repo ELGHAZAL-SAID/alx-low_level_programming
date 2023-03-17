941beb8f919dfe6976ffe309ee015ba166272d90
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-17 21:47:48 +0100

    putchar 0->9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+main - main function => 0->9 using putchar
+*
+*Return: return 0 
+*/
+
+int main(void)
+{
+	
+	int ch;	
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return 0;
+
+}
+
