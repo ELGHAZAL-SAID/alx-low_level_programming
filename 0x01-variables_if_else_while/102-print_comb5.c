426778e4e0236d5ac31d8d0f227ea8170f141985
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-18 01:25:03 +0100

    putchar possible combinations of two two-digit numbers.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,30 +8,32 @@
 
 int main(void)
 {
-    int ch, chr, cht, chs;
-    for (ch = 0; ch < 100; ch++)
-    {
-        for (chr = 0; chr < 100; chr++)
-        {
-            cht = ch * 100 + chr;
-            chs = chr * 100 + ch;
-            if (cht < chs)
-            {
-                putchar((ch / 10) + '0');
-                putchar((ch % 10) + '0');
-                putchar(' ');
-                putchar((chr / 10) + '0');
-                putchar((chr % 10) + '0');
-                if (ch != 98 || chr != 99)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-    putchar('\n');
-    return 0;
+
+	int ch, chr, cht, chs;
+
+	for (ch = 0; ch < 100; ch++)
+	{
+		for (chr = 0; chr < 100; chr++)
+		{
+			cht = ch * 100 + chr;
+        		chs = chr * 100 + ch;
+        		if (cht < chs)
+            		{
+                		putchar((ch / 10) + '0');
+                		putchar((ch % 10) + '0');
+                		putchar(' ');
+                		putchar((chr / 10) + '0');
+                		putchar((chr % 10) + '0');
+                		if (ch != 98 || chr != 99)
+                		{
+                    			putchar(',');
+					putchar(' ');
+                		}	
+            		}
+        	}
+    	}
+	putchar('\n');
+	return 0;
 
 }
 
