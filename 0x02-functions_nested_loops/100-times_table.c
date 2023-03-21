4c7f9e19c9efb2794f5e3001f4f7674367f9a18e
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-21 21:39:23 +0000

    print n times tible

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,7 +23,7 @@ void print_times_table(int n)
 			{
 				_putchar(mult / 100 + '0');
 				_putchar(mult % 100 + '0');
-				_putchar((mult % 100)/10 + '0');
+				_putchar((mult % 100)/10);
 			}
 			if (mult >= 10)
 			{
