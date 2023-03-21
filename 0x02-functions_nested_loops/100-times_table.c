8efd74e3173c6912dcaf0b21418c6ff242820d73
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-21 22:04:44 +0000

    print n times tible

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,13 +10,17 @@ void print_times_table(int n)
 {
 	int i, j, mult;
 
+	if (n = 0)
+	{
+		_putchar(48);
+		break;
+	}
 	for (i = 0; i <= n; i++)
 	{
 		_putchar(48);
 		for (j = 1; j <= n; j++)
 		{
 			_putchar(44);
-			_putchar(32);
 			_putchar(32);
 			mult = i * j;
 			if (mult >= 100)
@@ -27,11 +31,13 @@ void print_times_table(int n)
 			}
 			if (mult >= 10 && mult <= 99)
 			{
+				_putchar(32);
 				_putchar(mult / 10 + '0');
 				_putchar(mult % 10 + '0');
 			}
 			else
 			{
+				_putchar(32);
 				_putchar(32);
 				_putchar(mult + 48);
 			}
