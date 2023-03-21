73c74f1154817f6c4cbed9f0d417c9accd52f527
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-21 17:42:44 +0000

    printing 12h mn

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,11 +17,15 @@ void jack_bauer(void)
 			if (i < j)
 			{
 				if (i < 10)
+				{
 					_putchar(0 + '0');
-					_putchar(i + '0')
+					_putchar(i + '0');
+				}
 				else if (j < 10)
+				{
 					_putchar(0 + '0');
-					_putchar(j + '0')
+					_putchar(j + '0');
+				}
 				else
 				{
 					_putchar(i + '0');
