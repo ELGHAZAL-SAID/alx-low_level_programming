263979292146c15de898066c6daa652da52d025c
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-21 22:33:30 +0000

    sum of multiples of 3 && 5 bellow 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main - some of mpultiples of 3 and 5 below 1024
+*
+*Return: return 0
+*/
+
+int main(void)
+{
+	int i, num, sum;
+
+	for (i = 3; i <= 1024; i++)
+	{
+		if (i % 3 && i % 5)
+			sum += i;
+		else if (i % 3)
+			sum += i;
+		else if (i % 5)
+			sum += i;
+	}
+	printf('%d\n', sum);
+}
