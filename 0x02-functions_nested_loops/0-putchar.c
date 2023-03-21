b0626da41a9d42abdc1592bcddd5eb9efc524eb8
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-21 13:39:58 +0000

    printing using _putchar function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "_putchar.c"
+
+/**
+*main - print _putchar
+*
+*Retrun: always 0
+*/
+
+void main()
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+}
