465355f028ab73120b81f5635f87085ecd21e715
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-21 23:00:08 +0000

    print first 50 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+*main - print first 50 Fibonacci numbers
+*
+*Return - return 0
+*
+*/
+
+int main(void)
+{
+
+	int cnt, num, num1, num2;
+
+	num = 0; 
+	cnt = 0;
+	num1 = 0;
+	num2 = 1;
+	printf("%d, %d ", num1, num2);
+	while (cnt < 50)
+	{
+		num = num1 + num2;
+		printf("%d, ", num);
+		num1 = num2;
+		num2 = num;
+		cnt++;
+	}
+	return (0);
+}
