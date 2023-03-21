0b95fc3abdb09b4c7dbd7e164460bebf5bde0820
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-21 16:00:51 +0000

    printing alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*print_alphabet - print alphabet in lowercase a -> z
+*Return: return 0 always
+*/
+
+void print_alphabet()
+{
+
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++) 
+		_putchar(i);
+	_putchar('\n');
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef main_H
 #define main_H
 
-
 int _putchar(char c);
+void print_alphabet(void);
 
 #endif 
