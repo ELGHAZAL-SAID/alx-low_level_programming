f88c42f4110564cd888212e87b5a6127dfd81fa6
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-23 15:04:45 +0000

    print form 1 to 14 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*more_numbers - print 1 to 14, 10 times
+*
+*Return: nothing
+*/
+
+void more_numbers(void)
+{
+	int i, j, k;
+
+	i = 0;
+	while (i < 10)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (i >= 10)
+			{
+				_putchar(i / 10 + 32);
+				_putchar(i % 10 + 32);
+			}
+			_putchar(i + 32);
+		}
+	_putchar('\n');	
+	}
+
+}
