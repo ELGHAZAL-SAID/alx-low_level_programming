165277b09d941601f907cff7fe4fd7316af6130f
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-23 14:48:23 +0000

    print most of numbers 9 -> 0

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,9 +10,9 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 48; i <= 57; i++)
 	{
-		if ((i == 2) || (i == 4))
+		if ((i == 50) || (i == 52))
 		{
 			continue;
 		}
@@ -20,4 +20,3 @@ void print_most_numbers(void)
 	}
 	_putchar(10);
 }
-
