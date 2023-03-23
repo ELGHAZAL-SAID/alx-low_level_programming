0f818e863b77f496406e2533061fec9713a3fc7a
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-23 17:01:38 +0000

    fizz buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+*main - print nums form  1 to 100 and replace mult of 3 by Fizz and mult of 5 by Buzz
+*and mult of 3 and 5 by FizzBuzz
+*Return: always 1
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else
+			print("%d", i);
+		printf(" ");
+	}
+	printf("\n");
+	return (1);
+}
