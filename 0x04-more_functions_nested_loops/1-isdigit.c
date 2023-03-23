3ea3035caff904dbe35caa858143c7161f8ec7f9
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-23 12:53:36 +0000

    checking for digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_isdigit - check if c is digit
+*@c: var to check
+*Return: 1 if digit, else 0
+*/
+
+int _isdigit(int c)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (c == i)
+			return (1);
+	}
+	return (0);
+}
