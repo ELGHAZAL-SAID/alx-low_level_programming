b4b2f9a9f2abf958f0abf516b6eeede41d44fb14
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-27 23:58:42 +0000

    print a half of a string

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*print_array - print array elements
+*@n: number of elements to print
+*@a: array to be printed
+*Return: nothing
+*/
+
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		print("%d", a);
+		if (_strlen(a) != i + 1)
+			printf(", ");
+	}
+	printf("\n");
+}
