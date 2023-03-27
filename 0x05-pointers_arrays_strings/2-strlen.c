7e4aa681a4378ac431246cbd77ad5f6940c9cd6d
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-27 16:38:09 +0000

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_strlen - return length of string
+*@s: string to get its length
+*Return: length fo s
+*/
+
+int _strlen(char *s)
+{
+	return (sizeof(*s));
+}
+
