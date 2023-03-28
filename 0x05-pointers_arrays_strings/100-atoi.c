5e8fda8b29e81db650cb1eb2cdec49cffef9c302
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-28 01:18:40 +0000

    convert string to int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,25 +2,26 @@
 #include <stdlib.h>
 
 /**
-*_atoi - convert string to interger
-*@s: input to convert
-*Retrun: 0 if not int else interger found
+* _atoi - convert string to interger
+* @s: input to convert
+* Return: 0 if not int else interger found
 */
 
 int _atoi(char *s)
 {
+
 	unsigned int num = 0;
 	int num_sign = 1;
 
-	do
-	{
+	do {
 		if (*s == '-')
 			num_sign *= -1;
 		else if (*s >= '0' && *s <= '9')
-			num = (num * 10) + (*s - '0');
+			num = atoi(*s);
 		else if (num > 0)
 			break;
-	}
-	while(*s++);
+	} while (*s++);
+
 	return (num * num_sign);
 }
+
