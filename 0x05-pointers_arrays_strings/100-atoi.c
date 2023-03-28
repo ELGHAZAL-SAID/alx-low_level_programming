9ec0cc082f821f0b4c2e58d702c8ea422bb55b54
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-28 01:52:49 +0000

    convert string to int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,14 +13,16 @@ int _atoi(char *s)
 	unsigned int num = 0;
 	int num_sign = 1;
 
-	do {
+	while (*s != '\0')
+	{
 		if (*s == '-')
 			num_sign *= -1;
 		else if (*s >= '0' && *s <= '9')
-			num = (num * 10) + atoi(s);
+			num = (num * 10) + *s - '0';
 		else if (num > 0)
 			break;
-	} while (*s++);
+		*s++;
+	}
 
 	return (num * num_sign);
 }
