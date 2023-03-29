d9ef876f73e404360144c4049f434257a98f943c
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-29 16:58:25 +0000

    concatinating two tables

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* *_strncat - concatinates two strings
+* @dest: vat to append to
+* @src: var to be appended to
+* @n: number of chars to append
+* Return: dest var
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+	while (j < n)
+	{
+		*(dest + i) = *(src + j);
+		j++;
+		i++;
+	}
+
+	return (dest);
+
+}
