20cdd069a2e9085aa77689cd5a63389da834cf8d
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-29 23:40:58 +0000

    copying string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* *_strncat - concatinates two strings
+* @dest: vat to append to
+* @src: var to be appended to
+* @n: number of chars to append
+* Return: dest var
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && *(src + i) != 0)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	return (dest);
+
+}
