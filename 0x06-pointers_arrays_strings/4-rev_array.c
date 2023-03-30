e783f56188f43a5cd823b26b3a33ce8df60efad8
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-30 02:32:14 +0000

    reverse integer array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+* reverse_array - reverse an array of integers
+* @a: array to reverse
+* @n: length of the array
+* Return: always nothing
+*/
+
+
+void reverse_array(int *a, int n)
+{
+
+	int tmp = 0, i = 0, len = 0;
+
+	while (i <= len)
+	{
+		n--;
+		tmp = *(a + i);
+		*(a + i) = *(a + n);
+		*(a + n) = tmp;
+		i++;
+	}
+}
