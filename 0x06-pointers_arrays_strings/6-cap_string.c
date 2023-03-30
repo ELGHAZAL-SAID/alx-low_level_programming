9b85327c149d934dd4ace1262ce6a409b884e31b
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-30 03:44:46 +0000

    reverse integer array

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+
+/**
+* *cap_string - capitlize words
+* @s: string
+* Return: stirng
+*/
+
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	char t[] = {32, 9, 59, 10, 44, 46, 63, 33, 34, 40, 41, 123, 125};
+	char *x;
+
+	x = t;
+	while (*(s + i) != 0)
+	{
+		for (int j = 0; j < sizeof(t); j++)
+		{
+			if (*(s + i) == *(x + j))
+			{
+				if (*(s + (i + 1)) >= 97 && *(s + (i + 1)) <= 122)
+					*(s + (i + 1)) = *(s + (i + 1)) - 32;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
