85004092d42551b3be23443592646b19094b2fcf
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-03-30 01:52:30 +0000

    comparing two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -26,11 +26,12 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (*(s1 + i) > *(s2 + i))
 		{
-			return (15);
+			return (*(s1 + i) - *(s2 + i));
 		}
 		else if (*(s1 + i) < *(s2 + i))
 		{
-			return (-15);
+			
+			return (*(s1 + i) - *(s2 + i));
 		}
 	}
 
