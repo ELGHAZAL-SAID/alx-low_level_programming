b6cef677831970c768d1a06ef61726de276b02fa
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-04-03 17:58:37 +0000

    locate a char in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,18 +9,12 @@
 
 char *_strchr(char *s, char c)
 {
-	unsigned int i = 0;
+	unsigned int i;
+	unsigned int len = sizeof(s);
 
-	while (*(s + i) != 0)
-	{
+	for (i = 0; i <= len; i++)
 		if (*(s + i) == c)
-		{
 			return (s + i);
-		}
-		i++;
-	}
-	if (*(s + i) == c)
-		return (s + i);
 	return (0);
 
 }
