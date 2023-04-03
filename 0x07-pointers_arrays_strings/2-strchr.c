b4e4843ea31d13caaddb0f2db31adcef24adca4d
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-04-03 16:44:25 +0000

    copy a momoty area.

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* *_strchr - locate a character in a string
+* @s: string to search in
+* @c: char to lookfor
+* Return: char c
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	int len = sizeof(s);
+
+	for (i = 0; i < len; i++)
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	}
+	return (0);
+
+}
