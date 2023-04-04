eba66bf27d1b34d5cb620e00049c725c38a496eb
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-04-04 01:02:46 +0000

    pointer of a pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* set_string -  sets the value of a pointer to a char.
+* @s: pointer of a pointer
+* @to: to point to
+* Return: always 0
+*/
+
+
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
