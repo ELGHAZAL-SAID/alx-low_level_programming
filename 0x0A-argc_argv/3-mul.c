fffa26c322e7e43c052b0f66a695d1503943b0d8
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-04-08 17:36:44 +0000

    multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 /**
 * main - multiplies two numbers
@@ -8,4 +8,14 @@
 * Return: laways 0
 */
 
-
+int main(int argc, char *argv[])
+{
+	if (argc >= 3)
+		printf("%d\n", atoi(*(argv + 1)) * atoi(*(argv + 2)));
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
