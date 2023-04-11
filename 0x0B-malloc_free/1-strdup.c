3d0e96b721815bf672d9daad83ae6989577ee998
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-04-11 22:32:32 +0000

    getting right size

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,15 +12,20 @@ char *_strdup(char *str)
 	char *t;
 	int i;
 
+	while (*(str + i) != 0)
+		i++;
+
 	if (str == 0)
 		return (0);
-	t = malloc(sizeof(str));
+	t = malloc(i);
 
 	if (t == 0)
 		return (0);
+
 	for (i = 0; *(str + i) != 0; i++)
 	{
 		*(t + i) = *(str + i);
 	}
 	return (t);
 }
+
