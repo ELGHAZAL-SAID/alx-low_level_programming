2ae5288c3fbfcdeabe1708e73e0da6ffbcffdb7a
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-04-11 20:58:14 +0000

    checking malloc value

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,8 @@ char *create_array(unsigned int size, char c)
 	if (size > 0)
 	{
 		t = malloc(sizeof(t) * size);
+		if (t == 0)
+			return (0);
 		for (i = 0; i < size; i++)
 			*(t + i) = c;
 		return (t);
