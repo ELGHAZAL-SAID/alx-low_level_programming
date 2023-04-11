e11811946809d5bd6070674361f9bb252280aa97
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-04-11 20:51:14 +0000

    initializing array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* create_array - create an array
+* @size: size of the array
+* @c: char used to initialize the array
+* Return: 0 if size null,else pointer to array
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *t;
+	int i;
+
+	if (size > 0)
+	{
+		t = malloc(sizeof(t) * size);
+		for (i = 0; i < size; i++)
+			*(t + i) = c;
+		return (t);
+	}
+	return (0);
+}
