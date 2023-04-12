3a80c203949f263739f71dea5c53883a69c635be
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-04-12 02:03:07 +0000

    return pointer to 2D array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* alloc_grid - free 2 dimensional array
+* @width: width of the array
+* @height: height of the array
+* Return: pointer to demensional array
+*/
+
+
+
+int **alloc_grid(int width, int height)
+{
+	int  **t;
+	int i = 0, j = 0;
+
+	if (width <= 0 || height <= 0)
+		return (0);
+
+	t = (int **) malloc(sizeof(int *) * height);
+
+	if (!t)
+		return (0);
+
+	while (i < width)
+	{
+		t[i] = (int *) malloc(sizeof(int *) * width);
+		while (j < height)
+		{
+			*(*(t + i) + j) = 0;
+			j++;
+		}
+		i++;
+	}
+	return (t);
+
+}
