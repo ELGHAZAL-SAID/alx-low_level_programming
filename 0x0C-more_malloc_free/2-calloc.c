6e665b7740498f370435d202f1331896636c947f
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-04-13 18:15:23 +0000

    allocating memory for an array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,10 +15,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (0);
+
 	t = malloc(nmemb * size);
+
 	if (!t)
 		return (0);
-	for (i = 0; *(t + i); i++)
+
+	for (i = 0; i < (nmemb * size); i++)
 		*(t + i) = 0;
+
 	return (t);
 }
