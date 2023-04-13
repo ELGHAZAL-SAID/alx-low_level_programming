de1ef05c7c45f0274a29db8dc47a1d55570c6423
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-04-13 17:27:06 +0000

    concatinating two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,19 +15,23 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0, j = 0, len = 0;
 
 	if (s1 != 0)
+	{
 		while (*(s1 + i) != 0)
-			i++;
+		i++;
+	}
 	len = i;
 
 	if (s2 != 0)
-		while (*(s2 + j))
+	{
+		while (*(s2 + j) != 0)
 			j++;
+	}
 	if (n >= j)
 		len += j;
 	else
 		len += n;
 
-	t = malloc(sizeof(char) * (len + 1));
+	t = malloc(sizeof(char) * len);
 
 	if (t == 0)
 		return (0);
--- 0x0C-more_malloc_free/main.c
+++ 0x0C-more_malloc_free/main.c
@@ -11,7 +11,7 @@ int main(void)
 {
     char *concat;
 
-    concat = string_nconcat(NULL, "said", 99);
+    concat = string_nconcat("said el ghazal test this is just shit of him and her and what and and end ", "said wtf s going on", 3);
     printf("%s\n", concat);
     free(concat);
     return (0);
