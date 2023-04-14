6c6cd0b49c6739c7c479b50f42f5d2e2845a79f8
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-04-14 00:28:55 +0000

    resizing an array

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -27,7 +27,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		t = malloc(new_size);
 
 		if (t == 0)
+		{
+			free(ptr);
 			return (0);
+		}
 		t = memcpy(t, ptr, old_size);
 		free(ptr);
 	}
@@ -36,7 +39,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		t = malloc(new_size);
 
 		if (t == 0)
+		{
+			free(ptr);
 			return (0);
+		}
 		t = memcpy(t, ptr, new_size);
 		free(ptr);
 	}
