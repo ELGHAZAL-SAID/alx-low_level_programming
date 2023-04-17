5cb27bb78f6ecebc844b13da70c96c0df9a3d52d
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-04-17 22:01:38 +0000

    create new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 
-
 /**
 * new_dog - create now dog
 * @name: dog name
@@ -12,14 +11,42 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *d;
+	int i = 0, len_name, len_owner;
 
 	d = malloc(sizeof(struct dog));
 
-	if (d == 0)
+	if (d == 0 || !(name) || !(owner))
+	{
+		free(d);
 		return (0);
+	}
 
-	(*d).name = name;
+	while (*(name + i))
+		i++;
+	len_name = i;
+
+	i = 0;
+	while (*(name + i))
+		i++;
+	len_owner = i;
+
+	(*d).name = malloc(len_name + 1);
+	(*d).owner = malloc(len_owner + 1);
+
+	if (!((*d).name) || !((*d).owner))
+	{
+		free(d);
+		free((*d).name);
+		free((*d).owner);
+		return (0);
+	}
+	for (i = 0; i <= len_name; i++)
+		(*d).name[i] = name[i];
+
 	(*d).age = age;
-	(*d).owner = owner;
+
+	for (i = 0; i <= len_owner; i++)
+		(*d).owner = owner;
 	return (d);
+
 }
