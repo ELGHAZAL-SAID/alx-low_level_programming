72580a0cb9057dcc83b3f5cdec118c884ac60887
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-04-20 18:24:37 +0000

    print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+* print_numbers - print numbers
+* @separator: to be printed between numbers
+* @n: number of integers
+* Return: 0
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list arg;
+
+	va_start(arg, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(arg, int));
+		if (i == n - 1)
+			continue;
+		printf("%s", separator);
+	}
+	printf("\n");
+	va_end(arg);
+}
