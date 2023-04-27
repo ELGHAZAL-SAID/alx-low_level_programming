8e251d664493f8df3f31a3b5a3aca83b0d0d80c7
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-04-27 16:51:32 +0100

    add node the the beginning

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+* add_node - add new node to the beginneg of the linked list
+* @head: linked list
+* @str:value to add
+* Return: address of the new element added else NULL
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *tmp = *head;
+
+
+	if (str == 0)
+		return (0);
+	tmp->str = strdup(str);
+	tmp->len = strlen(str);
+	tmp->next = *head;
+
+	*head = tmp;
+
+	return (*head);
+}
