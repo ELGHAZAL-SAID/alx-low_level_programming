46147821ad120f34da50380454b78c1af1905116
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-04-28 23:55:07 +0100

    return the lenght of a linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* listint_len - returns the len of a linked list 
+* @h: linked list
+* Return: 0
+*
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	int i = 0;
+
+	while (h != 0)
+	{
+		i++;
+		h = h->next;
+	}
+	return(i);
+}
