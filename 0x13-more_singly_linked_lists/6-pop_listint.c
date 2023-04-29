6ab4225e5ecace34d924e2f51164d13110f4844b
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-04-29 16:38:17 +0100

    pop a node from a list

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,14 +9,16 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *p;
+	int n;
 
 	if (*head == 0)
 		return (0);
 
-	p = *head;
+	p = (*head)->next;
+	n = (*head)->n;
 	free(*head);
-	*head = p->next;
+	*head = p;
 
-	return (p->n);
+	return (n);
 
 }
