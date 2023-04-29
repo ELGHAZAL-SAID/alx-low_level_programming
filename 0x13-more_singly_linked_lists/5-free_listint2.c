543928a03130ba1f28de2e4db0573c467503e96d
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-04-29 14:40:46 +0100

    fixing bugs - free()

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *p;
 
+	if (head == NULL)
+		return;
+
 	while ((*head) != 0)
 	{
 		p = (*head)->next;
 		free(*head);
 		(*head) = p;
 	}
-	*head = 0;
+	*head = NULL;
 }
