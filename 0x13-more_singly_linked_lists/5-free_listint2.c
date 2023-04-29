5e6796bb7f253ff2c82c468d31cb0979451555aa
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-04-29 01:19:39 +0100

    free list and set the head to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* free_listint2 - free a linked list
+* @head: pointer to pointer of a linked list
+* Return: nothing
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *p;
+
+	while ((*head) != 0)
+	{
+		p = (*head)->next;
+		free(*head);
+		(*head) = p;
+	}
+	*head = 0;
+}
