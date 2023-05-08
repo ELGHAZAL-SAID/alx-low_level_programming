62aa71217f58a74ef4e0293353d1ae45c73c8886
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-05-08 23:01:41 +0100

    [Updates] fix betty coding style p

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
-* append_test_to_file - append text to the end of a file
+* append_text_to_file - append text to the end of a file
 * @filename: file name
 * @text_content: content of the file
-* Return: 1 if success else 0 
-*
+* Return: 1 if success else 0
 */
 
 
