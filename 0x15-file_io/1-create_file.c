8f31fd13692574b1673ca2058bccf98d90ae6935
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-05-08 22:30:41 +0100

    [Updates] fix some bugs

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == 0)
 		return (-1);
 
-	id = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
+	id = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (id == -1)
 		return (-1);
