f14460b59e17a17048328bfe3aaef7506eebeda4
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-05-08 22:22:51 +0100

    creating file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* create_file - creates a file
+* @filename: file name
+* @text_content: content to put in the file
+* Return: 1 if success, else -1
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int id, w;
+
+	if (filename == 0)
+		return (0);
+
+	id = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
+
+	if (id == -1)
+		return (-1);
+
+	if (text_content != 0)
+	{
+		w = write(id, text_content, strlen(text_content));
+		if (w == -1)
+			return (0);
+	}
+
+	return (1);
+}
